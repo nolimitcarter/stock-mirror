84b1526888647c89127faea2e1b6208b24f04ba1
nolimitcarter <carterthayer@protonmail.com>
2021-02-10 17:12:13 -0500

    added

---
+++ stockmirror.py
@@ -0,0 +1,15 @@
+## stockmirror.py 
+## This program was made by @nolimitcarter & @brodieschaefers
+
+from Tkinter import *
+import locale
+import thredding 
+import time 
+import requests
+import json
+import traceback
+import feedparser
+
+from PIL import Image, ImageTk
+from contextlib import contextmanager
+
